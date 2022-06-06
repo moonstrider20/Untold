f3c2244ac4b21a91adca8cd13ed3225d3e3fae40
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2022-06-06 08:09:02 -0400

    Untold 1.3
    Insert Game Loop

--- Untold/Program.cs
+++ Untold/Program.cs
@@ -8,9 +8,34 @@ namespace Untold
         {
             Console.WriteLine("Welcome to Untold!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                };
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
