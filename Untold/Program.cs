b914eb1da58663f2f57ffe1f2717db55764e38c3
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2022-06-06 07:20:53 -0400

    Untold 1.1
    Look and Quit

--- Untold/Program.cs
+++ Untold/Program.cs
@@ -6,7 +6,22 @@ namespace Untold
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome!");
+            Console.WriteLine("Welcome to Untold!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
