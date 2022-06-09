fbe0f4df7390696ba2a3823645aee3ad611ad765
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2022-06-09 15:41:57 -0400

    Untold 2.1 - Start Game World
    Made array so player can move west and east.

--- Untold/Program.cs
+++ Untold/Program.cs
@@ -4,6 +4,7 @@ namespace Untold
 {
     class Program
     {
+        private static string Location => Rooms[locationColumn];
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Untold!");
@@ -11,23 +12,23 @@ namespace Untold
             Commands command = Commands.UNKOWN;
             while (command != Commands.QUIT)
             {
-                Console.Write("> ");
+                Console.Write($"{Location}\n> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
                 switch (command)
                 {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-                    case Commands.LOOK:
-                        outputString = "This is an open field west of a white house.";
-                        break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}";
+                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house.";
+                        break;
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
                         break;
                     default:
                         outputString = "Unknown command.";
@@ -39,5 +40,30 @@ namespace Untold
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+                case Commands.EAST when locationColumn < Rooms.Length - 1:
+                    locationColumn++;
+                    didMove = true;
+                    break;
+                case Commands.WEST when locationColumn > 0:
+                    locationColumn--;
+                    didMove = true;
+                    break;
+            }
+
+            return didMove;
+        }
+
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int locationColumn = 1;
     }
 }
